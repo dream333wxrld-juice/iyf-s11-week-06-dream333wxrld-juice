f0321964d71de4f16bb5c879ae2e16a64763b788
Gilbert <dream333wxrld@gmail.com>
2026-07-21 00:28:18 +0300

    feat: add Task 11.2 callback hell and promises

--- async-practice.js
+++ async-practice.js
@@ -51,3 +51,104 @@ function loadUser(userId, callback) {
 loadUser(1, function(user) {
     console.log("User loaded:", user);
 });
+
+// Task 11.2: Callback Hell & Introduction to Promises
+
+// Exercise 1: Callback Hell (the bad way)
+function getUserData(userId, callback) {
+    setTimeout(() => {
+        callback({ id: userId, name: "John" });
+    }, 1000);
+}
+
+function getUserPosts(userId, callback) {
+    setTimeout(() => {
+        callback([
+            { id: 1, title: "Post 1" },
+            { id: 2, title: "Post 2" }
+        ]);
+    }, 1000);
+}
+
+function getPostComments(postId, callback) {
+    setTimeout(() => {
+        callback([
+            { id: 1, text: "Great post!" },
+            { id: 2, text: "Thanks for sharing" }
+        ]);
+    }, 1000);
+}
+
+// The nightmare - callback hell demonstration
+getUserData(1, function(user) {
+    console.log("User:", user);
+    getUserPosts(user.id, function(posts) {
+        console.log("Posts:", posts);
+        getPostComments(posts[0].id, function(comments) {
+            console.log("Comments:", comments);
+        });
+    });
+});
+
+// Exercise 2: Promises to the Rescue
+const myPromise = new Promise((resolve, reject) => {
+    const success = true;
+    
+    setTimeout(() => {
+        if (success) {
+            resolve("It worked!");
+        } else {
+            reject("Something went wrong");
+        }
+    }, 1000);
+});
+
+myPromise
+    .then(result => {
+        console.log("Success:", result);
+    })
+    .catch(error => {
+        console.log("Error:", error);
+    });
+
+// Refactor getUserData to return a Promise
+function getUserDataPromise(userId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userId > 0) {
+                resolve({ id: userId, name: "John" });
+            } else {
+                reject("Invalid user ID");
+            }
+        }, 1000);
+    });
+}
+
+// Refactor getUserPosts to return a Promise
+function getUserPostsPromise(userId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                { id: 1, title: "Post 1" },
+                { id: 2, title: "Post 2" }
+            ]);
+        }, 1000);
+    });
+}
+
+// Refactor getPostComments to return a Promise
+function getPostCommentsPromise(postId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([
+                { id: 1, text: "Great post!" },
+                { id: 2, text: "Thanks for sharing" }
+            ]);
+        }, 1000);
+    });
+}
+
+// Test the Promise versions
+getUserDataPromise(1)
+    .then(user => console.log("Promise version - User:", user))
+    .catch(error => console.error(error));
